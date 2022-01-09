77621d31e2457caf55a72da4ab56029d7a14b179
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-09 14:04:46 +0530

    Add files via upload

---
+++ 02_Introducing_git/02_04_History_Of_Git.js
@@ -0,0 +1,33 @@
+// Note: This content is not that importent
+
+// Linus Torvalds invented git. 
+
+// He is the main person behind in the creation of Linux also 
+
+
+// before git, he was working in a version control system called 
+// * BitKeeper *. Due to some disagreements the users of BitKeeper
+// didnot allow Linus Torvalds to use free version and they wanted 
+// him to pay money 
+
+// This became little contraversial 
+
+// So Linus wanted to create something fast and open source 
+// and he dedicated the month of April 2005 to create git and got 
+// the basic functionalites ready.
+
+
+
+
+// Reason behind the name git: 
+
+
+// Tarvalds named git as 'The stupid content tracker' while he was working on 
+// it then he chosed the name git. 
+
+// The official Git Source code explains a couple different meanings for the name, 
+// depending on your mood 
+
+
+
+
