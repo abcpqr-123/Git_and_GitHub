47045ad417e38e2d2d415746240a173540ef04f3
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-09 14:05:19 +0530

    Add files via upload

---
+++ 02_Introducing_git/02_06_Git_vs_GitHub.js
@@ -0,0 +1,18 @@
+
+// GIT IS NOT GIUTHB
+
+
+// Git: 
+// Git is the version control softwere that runs locally on ur machine 
+// You dont need to register for an account. 
+// You don't need to register for an account. 
+// You don't need the internet to use it 
+// You can use git without ever touching github 
+
+
+
+// Github: 
+// Github is asevice that hosts Git repositories in the cloud and makes it 
+// easier toi collaborate with ohter people.
+// You do need to sign up for a account to use Github
+// Its an online place to share work that is done using git 
