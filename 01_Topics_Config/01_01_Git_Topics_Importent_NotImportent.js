27b7b82e00e9037fd5214d6395eeeba80588a831
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-12 12:35:20 +0530

    Add files via upload

---
+++ 01_Topics_Config/01_01_Git_Topics_Importent_NotImportent.js
@@ -0,0 +1,51 @@
+// Very Importent: 
+
+// * Intro to Git 
+
+// * Installation 
+
+// * Git Basics 
+
+// * Committing in Detail 
+
+// * Branching 
+
+// * Merging 
+// //-----------------------------
+// Very Importent but not improtant in day to day github usage: 
+
+// # Diffing 
+
+// # Stashing 
+
+// # Undoing Changes 
+// //-----------------------------
+
+
+// Very Importent topics of github 
+
+// * Github Intro 
+
+// * Fetching & Pulling 
+
+// * Github Odds & Ends 
+
+// * Collborative Workflows 
+
+// Not that improtent, still need to learn
+
+// # Rebasing 
+
+// # Interactive Rebasing 
+
+// # Git Tags 
+
+// # Git Behind The scenes 
+
+// # Reflogs 
+
+// # Custom Aliases 
+
+
+
+
