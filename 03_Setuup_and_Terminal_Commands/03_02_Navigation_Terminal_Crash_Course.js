83521cb3a48995c461cc603a767306fec27a95f2
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-12 12:33:57 +0530

    Add files via upload

---
+++ 03_Setuup_and_Terminal_Commands/03_02_Navigation_Terminal_Crash_Course.js
@@ -0,0 +1,64 @@
+// *  ls ==>  It is shortform for list 
+//          Used to show all the files of that folder folder.
+
+
+
+
+// *   start . ==> This will open the file explorer for windows 
+//     open . ==> This will open the file explorer Mac 
+  
+
+
+
+// * clear  ==> used to clear the terminal 
+
+
+
+
+// * ls Music/  ==> this will show what all the files are there in the Music 
+//                  folder. 
+
+
+// Note: You have to define the proper path to access a perticular folder 
+//       u cnnot accees by simply typing the name of that folder 
+
+//       Ex: If there is a floder inside music folder called India_music, then 
+//           u hagve to type: 
+
+//           ls Music/Indian_music 
+
+//           then only u wiil be able to access the Indian_music file 
+
+
+
+// Note: This topic is in third deleting files and folders Vid 
+
+//  ls -a  ==> used to view all the hidden files like .DS_Store etc 
+//             Hidden files comes with a dot(.) In the begging 
+
+
+
+// * pwd   ==> Print working directory
+//             It prints the current directory of folder in which u r  working
+
+
+// * cd    ==> Is used to change the directory 
+
+
+// * cd ..  ==> is used to move from current folder to ONE LEVEL BACK 
+ 
+
+
+// Note: This topic is in third deleting files and folders Vid 
+
+//  ls -a  ==> used to view all the hidden files like .DS_Store etc 
+//             Hidden files comes with a dot(.) In the begging 
+
+
+
+
+
+
+
+
+
