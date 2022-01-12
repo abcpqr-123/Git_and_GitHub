f07db1607d39f4604783b94bcab1d4b6a27292fb
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-12 12:34:19 +0530

    Add files via upload

---
+++ 03_Setuup_and_Terminal_Commands/03_03_Creating_Files_and_Folders.js
@@ -0,0 +1,51 @@
+// CREATE FILES:
+// * touch  ==> Used to create a new file inside a directory 
+                // Ex: If u want to create a new file - called app.js 
+                //     then u can write: 
+                // touch app.js 
+                
+                // app.js file will be created 
+
+    //   Note: u can create MULTIPLE FILES by writing: 
+
+    //   touch app1.js app2.js app3.js app4.js 
+
+
+    // # If u want to create a file in a specific folder and u r not in that 
+    //   folder then, u have mention the specific path of the folder in which
+    //   u want 
+    //    Ex: touch Desktop/Local_Storage/NewFile.js 
+              
+
+
+
+
+
+
+
+// CREATE FOLDERS:
+
+// Note: While creating folders its recommended that u shoud not leave 
+//       spaces between the worlds 
+
+//       In case if u have then u can write like this: 
+//       Ex: "New Folder"
+
+// mkdir ==>  to create folders we do use mkdir means, make directory 
+
+//            Ex: mkdir NewFolderGit 
+
+// # If u want to create a sub folder in the folder in which u r not there 
+//   right now, then u have to define complete path of that folder 
+
+//   For Exmple, if u want to create a folder in desktop then     
+//        Ex 1: mkdir Desktop/Git-Practic"new folder"
+//        Ex 2: mkdir "new folder"/folder1/folder2
+
+
+
+
+// multiple folders: 
+
+// U can create multiple folders at a time
+// Ex: mkdir folder1 folder2 folder3 folder4
