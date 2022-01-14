814f751c12811e2b556eeb750b142cd71077903f
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-14 09:03:48 +0530

    Add files via upload

---
+++ 04_The_Basicis_git-Adding_Commiting/04_04_The_Misterious-DOTGit_Folder.js
@@ -0,0 +1,29 @@
+// if u hit simply * ls * command the hidden .git folder will not appear 
+
+// however if u run * ls -a * u can see the hidden .git folder  
+// $ ls -a
+// \./  ../  .git/
+
+
+// If u want u can aslo * cd * into the .git folder. 
+// $ cd .git
+
+// Note: If u delete that .git folder then THE COMPLETE HISTORTY  WILL BE 
+//       DELETED FOR GOOD 
+
+// before deleteing git folder: 
+
+
+// ~/desktop/git-practice/FinalFileforTest (master)
+// $ git status
+// On branch master
+
+// No commits yet
+
+// nothing to commit (create/copy files and use "git add" to track)
+
+
+
+
+
+
