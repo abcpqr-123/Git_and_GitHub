7f9b72fde7bf2e7384f7922b1533ae511866280e
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-14 09:03:19 +0530

    Add files via upload

---
+++ 04_The_Basicis_git-Adding_Commiting/04_02_What_is_git_Repo.js
@@ -0,0 +1,11 @@
+// Repository: 
+
+// A Git "Repo" is a workspace which tarcks and mantges files withhin a folder 
+
+
+// Anytime we want to use Git with a project, app, etc we need to 
+// create a new git Repository. We can have as many repos on our
+// machine  we needed. all with separate histories and contents 
+
+
+
