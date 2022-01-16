5f814b0e3009cc1167684f5d6a98ba8e31562ca9
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-16 16:49:08 +0530

    Add files via upload

---
+++ 05_Commits_In_detail/05_06_Commiting_With_GUI.js
@@ -0,0 +1,80 @@
+// So first of all u have to open up the folder in other words repo 
+// in which u r working. 
+
+
+// now u make the changes in the file using the text editor 
+// and save it 
+// u can also add new files 
+
+
+// now when u hit git status, in git bash it will show u the changes 
+
+
+// And when it comes to GUI it will also show that  there is a  achange, 
+// and it will appear at the top 
+
+// if u added 2 new files and edited 1 file it will show 
+// -->  2 + 1 
+
+// and in the right part of the screen 
+// pincil sign --> edited 
+// + sign --> Newly created 
+
+
+// if u want to commit the newly created file, move the curser on that file 
+//  click statge 
+//  at this point u have staged 1 file 
+
+// now the file name will appear in the area of staged files
+
+//  if u hit git status in git bash 
+
+//  if will show: 
+
+//  chnages to be commited 
+//     modeifed: (name of the modified file)
+
+
+// Now u can type the commit message in GUI
+// IN THE RIGHT BOTTOM SIDE: 
+
+// type ur message
+
+// and click commit changes 
+
+
+// and u will see the new commit added to the list 
+
+
+// //----------------------
+
+
+// now if u want to commit oher two files which u edited 
+// then also same process 
+
+// click stage for both the files, 
+
+
+// now they will appear in the area of staged files 
+
+
+// type ur commit message 
+
+// Now, to test 
+// if u hit ==> git satatus in git bash it will show:  two untracked files 
+
+
+// now come back to GUI and click on commit changes 
+
+
+// now u will see the changed files commited in the list 
+
+
+// if u hit ==> git log --oneline in the git bash 
+// u can also see the one line commited message 
+
+
+
+
+
+
