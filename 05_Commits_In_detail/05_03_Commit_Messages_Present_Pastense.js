4d095d63a136504feecf17c8fd7afc72d7185d3d
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-16 16:48:15 +0530

    Add files via upload

---
+++ 05_Commits_In_detail/05_03_Commit_Messages_Present_Pastense.js
@@ -0,0 +1,26 @@
+// Writing COmmit Messages
+
+// Present or past tense? Does it really maters? 
+
+
+// Present-Tense Imperative style?? 
+
+// From the Git docs: 
+
+// Describe ur changes in inperative mood, e.g 
+// "make xyzzy do frontz"   instead of 
+// "makes xyzzy do frontz" or "I changed xyzzy to do frontz", 
+// as if u r giving orders to the codebase to change its behavior 
+
+
+// ///----------------------------------------------------------
+
+// IMP NOTE: 
+
+// Well finally it depends on the organization or the comapany in which 
+// u r working with 
+
+// if the instruction for ur team is to make changes in the present tense
+// then u make changes in preset tense 
+// if they ask u to do in past then u do it in past tense 
+
