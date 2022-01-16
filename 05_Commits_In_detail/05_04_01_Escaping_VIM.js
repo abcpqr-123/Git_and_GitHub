01015431d89bfed2bce1df83c6d3fa89719b2506
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-16 16:48:27 +0530

    Add files via upload

---
+++ 05_Commits_In_detail/05_04_01_Escaping_VIM.js
@@ -0,0 +1,37 @@
+// when we use git commit WITHOUT -m, it opens a default editor, and its 
+// likely to be VIM. 
+
+// if u type ==> git commit 
+// now, the VIM is open 
+
+// so once its open the first thing u have to do is type i --> STANDS FOR insert 
+// and hit enter. now u r in insert mode 
+
+// Now u can type ur insert message. 
+// Ex: fixed the typo. 
+
+// once u  r finised with ur message 
+
+// NOW U HAVE TO GET OUT OF WIM. TO DO THAT: 
+
+// u have to hit escape key once. by doing that u will leave the insert mode. 
+
+// Now, u have to leave vim entirly. to do that: 
+
+// u  have to  type it the bottom-->   :wq    here w stands for write 
+//                                                 q stands for quit 
+// So Basecally it means write and quit. 
+
+
+
+// and IMPORTENT NOTE: 
+
+// After completeing all the above steps if u r not able to type anything 
+// then u have to peress  --> q + Enter 
+
+// after that u will be able to enter any command u want. 
+
+
+
+
+
