d9c78c66eb0f333aa3818ab3ee5aa7de67abccba
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-16 16:48:52 +0530

    Add files via upload

---
+++ 05_Commits_In_detail/05_05_Closer_look_Git_Log.js
@@ -0,0 +1,23 @@
+// Some times we make very long commits which contiains many lines and 
+// sometimes even paragrphs 
+
+// when we hit ==> git log, it will be very hard to read all the messages
+// which appear 
+
+// so we use to FORMAT COMMITS 
+
+// Once again if u want then u can get more info about this in git documentation 
+// in git docs - git log 
+
+
+// THERE ARE MANY FORMAT COMMITS, few improtent commits are: 
+
+// ==> git log --abbrev-commit 
+
+// it shows less and only imortent commits in comparison to git log 
+// still its not engough, its better to git much more less info 
+// in one single line for that we do use: ==>: git log --oneline 
+
+
+// ==> git log --oneline 
+// Ex: remove extra blank lines in chapter2
