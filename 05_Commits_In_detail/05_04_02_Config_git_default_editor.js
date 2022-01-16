e2b60d573a0999627e4acdeeead7331111ddb232
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-16 16:48:40 +0530

    Add files via upload

---
+++ 05_Commits_In_detail/05_04_02_Config_git_default_editor.js
@@ -0,0 +1,45 @@
+// some times we have to type paragraphs as commit messages and to do that 
+// VIM is not that sutable 
+
+// so for that u can use the default  editor 
+
+// u can use any defalt editor. 
+
+// for VS Code(visual studio code) the command is down bellow. to set any other 
+// editor as default  editor,  u have to go to git documentation, there is a page called
+// "git commands set up and config"
+
+// and u can choose the command there. 
+
+//-------------------------
+
+// for VS code editor ==> git config --global core.editor "code --wait"
+
+// if u r in vs code copy the above command and paste it git bash 
+// u wont see any changes in git bash. 
+
+// so make chages in any file 
+
+// now instead of adding commit messeage in VIM u can add the message in VS CODE  
+// only. 
+
+// when u hit command  ==> git commit 
+// the file will get opened in the vs code only 
+
+// Steps to add the comment in VS CODE: 
+
+// * Type the message 
+
+// * close the file. NOTE that u cannot make commit untuil u close the file 
+
+//--------- 
+
+// --wait means  wait until in colose the Files.
+
+//--------------------
+
+// Now if u type git log u can see the commit message
+
+
+
+
