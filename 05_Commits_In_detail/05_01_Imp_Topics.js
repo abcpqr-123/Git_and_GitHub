d7b92457a0eee1076191af16b62746cad7d5dc98
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-16 16:47:48 +0530

    Add files via upload

---
+++ 05_Commits_In_detail/05_01_Imp_Topics.js
@@ -0,0 +1,41 @@
+// Critical: 
+
+// * Git Ingnore
+
+
+// //---------------------------------
+
+// Imprortant: 
+
+// * Writing Atomic commits
+
+// * Configuring default editor 
+
+// * Writing good commit messages 
+
+// * Navigating the Git Documentation 
+
+
+// //---------------------------------
+
+
+
+// Nice to have: 
+
+// * Working with a GUI 
+
+
+// * Amending Commits 
+
+
+
+// //---------------------------------
+
+
+
+
+
+
+
+
+
