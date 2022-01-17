2078a89f81902a9f8d9fc6a8b80a98c0a184c78d
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-17 16:39:52 +0530

    Add files via upload

---
+++ 06_Working_With_Branches/06_05_Git_Branch-view_Branch.js
@@ -0,0 +1,8 @@
+// Viewing Brances: 
+
+// ==> git branch 
+
+// Use ==> git branch to view ur existing branches. The default branch 
+// in every git repo is master, though u can cofigure this. 
+
+// Note: " * " indicates  the branch u r currently on 
