d5e0f28504baf203b5ad44968fb3a1a26ad260bb
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-17 16:38:18 +0530

    Add files via upload

---
+++ 06_Working_With_Branches/06_02_Introducing_Branches.js
@@ -0,0 +1,52 @@
+//  why u need git branching? 
+
+//  On large projects we oftern worj in multiple contexts.. 
+
+
+//  1. u r working on  2 different color schemes variations for 
+//     ur website at the same thime, ensure of which u like best 
+
+
+// 2. A teammate is also working on adding a new chat widget to present 
+//    at the next meeting. Its unclear if ur company will end up using 
+//    it. 
+
+// 3. Another coworker is upadating the search bar autocomplete. 
+
+// 4. Another developer is doing an experimental radiecal design 
+//    over the entire layout to present next month 
+
+
+// if they all work in linear fassion, one commit after the another it will 
+// be impossible. 
+
+// beacuse if someone is doing a radical experimental design change on every 
+// single view of our application, at that situation every thing get changes
+//       But if someone is trying to work on some minor color scheme variations, 
+//       or 
+//       if someone's workjing on adding a chat wisit to the bottom of our screen 
+// those, things need to happen in separate contexts beacuse 
+// they have implications on one anoteher and they need to happen in isolation until
+// maybe at some at some point we may deside to encoparate each ouhers changes at 
+// some point. 
+
+ 
+
+// so that is where branching comes in that is what git allows us to do 
+
+
+
+
+// Branches!: 
+
+// Branches are an essential part of git! 
+
+// Think of branches as alternative timelines for a project 
+
+// They enable us to create separate contexts where we can try new 
+// things, or even work on multiple ideas in parallel 
+
+// If we make changes on one branch, they do not impact the ohter 
+// branches(unless we merge the changes)
+
+
