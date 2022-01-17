10c5cfe0b45bcef53bfc999ec532ea9cd748fb66
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-17 16:36:21 +0530

    Add files via upload

---
+++ 06_Working_With_Branches/06_01_Imp_Topics.js
@@ -0,0 +1,32 @@
+// Critical: 
+
+// * Branching.. what is it? 
+
+// * Understanding Git HEAD 
+
+// * Git branch 
+
+// * Git switch 
+
+// * Git Checkout  
+
+// //---------------------------------
+
+// Imprortant: 
+
+// * Deleting & Renaming Branching
+
+// * Master vs. Main Branch  
+
+// //---------------------------------
+
+
+
+// Nice to have: 
+
+// * HEAD & Refs behind the senses
+
+
+
+// //---------------------------------
+
