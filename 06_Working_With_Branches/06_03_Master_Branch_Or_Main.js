8e3ba2f47463cecade51d55b5ff3f563b163a279
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-17 16:39:05 +0530

    Add files via upload

---
+++ 06_Working_With_Branches/06_03_Master_Branch_Or_Main.js
@@ -0,0 +1,27 @@
+// The master Branch: 
+
+// In git, we are always working on a branch The default branch name is "master"
+
+// It doesn't do anything special or have fancy powers. Its just like any
+// other branch. 
+
+
+
+//-----------
+
+// Master
+
+// Many people designate the master branch as their "source of truth" or "the official
+// branch"  for there code base, but that is left for u to decide. 
+
+// From the git perspective, the master branch is just like any other branch. It does 
+// not have to hold the "master copy" of ur project 
+
+
+//                     Master? 
+//                     Main? 
+
+// In 2020 Github renamed the default branch from master to main. but, 
+// the default Git branch name is still master, though the Git team is exploring 
+// a potential change. 
+
