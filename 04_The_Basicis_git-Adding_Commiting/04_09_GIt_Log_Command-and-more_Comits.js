26592b3fb0c6949bceba15664c28010f05fbc783
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-19 14:27:09 +0530

    Add files via upload

---
+++ 04_The_Basicis_git-Adding_Commiting/04_09_GIt_Log_Command-and-more_Comits.js
@@ -0,0 +1,22 @@
+// Note: when ever u make changes in the already committed file, git 
+//       detects the change and it shows that there is a modification 
+//       in that committed file
+
+// Ex: $ git status
+//     On branch master
+//     Changes not staged for commit:
+//     (use "git add <file>..." to update what will be committed)
+//     (use "git restore <file>..." to discard changes in working directory)
+//         modified:   outline.txt
+
+
+
+
+//--------------------------------------
+
+
+// git add . ==> 
+
+// Adding all changes: 
+
+// Use git add . to stage all changes at once 
