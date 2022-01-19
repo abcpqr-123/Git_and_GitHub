25665d8efbcf597f4ae6798913ff583c901f7277
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-19 14:23:41 +0530

    Add files via upload

---
+++ 03_Setuup_and_Terminal_Commands/03_01_What_Really_Imp.js
@@ -0,0 +1,18 @@
+// Critical topics: 
+
+// * The different ways of interacting with Git: Command Line & GUIs
+
+// * installing Git! 
+
+// * Conficuring Git Name and Email 
+
+// * Trminal Crash Course videos 
+
+
+
+
+// Importent topics: 
+
+// * Installing GitKraken 
+
+
