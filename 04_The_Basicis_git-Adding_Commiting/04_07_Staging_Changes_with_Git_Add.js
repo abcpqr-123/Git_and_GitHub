dd4b649e8c2bf38c5d57b3e56ba7b4409fee740f
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-19 14:26:34 +0530

    Add files via upload

---
+++ 04_The_Basicis_git-Adding_Commiting/04_07_Staging_Changes_with_Git_Add.js
@@ -0,0 +1,20 @@
+// Adding: 
+
+// Use git add to add specific files to the staging area. Swparate files 
+// with space to add multiple at once 
+
+
+// Git command for adding: ==> git add 
+
+// Ex: git add file1.txt file2.txt
+
+
+//-------------------
+
+
+// git add . ==> 
+
+// Adding all changes: 
+
+// Use git add . to stage all changes at once 
+// vid: The Git Log Command(A more Commiting) - 29 
