3e5671cbd9d4245370066c7f51a5454fb2163659
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-20 22:49:32 +0530

    Add files via upload

---
+++ 06_Working_With_Branches/06_12_Branching_Exercise.js
@@ -0,0 +1,22 @@
+// touch patronus.txt 
+// git add patronus.txt 
+// git commit -m "add empty patronus file"
+// git branch harry 
+// git branch snape 
+// git switch harry 
+// code . 
+// git add patronus.txt 
+// git git commit -m "add harry's stag patronus"
+// git log 
+// git chekout snape 
+// git add patronus.txt 
+// git commit -m "add snape's doe patronus"
+// git log 
+// git switch -c lily 
+// git log 
+// git status
+// git add patronus.txt 
+// git commit -m "add lily's doe patronus"
+// git branch 
+// git branch -d snape 
+// git branch 
