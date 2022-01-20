c5c0e33b0d3f010797d253499a417fe84d7ad7fd
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-20 22:49:20 +0530

    Add files via upload

---
+++ 06_Working_With_Branches/06_11_howGitStore_Head-&-Branches.js
@@ -0,0 +1,14 @@
+// HEAD --> master 
+
+// HEAD: 
+
+// we'll often come across the tearm HEAD in git 
+
+// HEAD is simply a pointer that refers to the current "location" in ur 
+// repository. It points to a particular branch reference. 
+
+// It always points the tip of the barnch 
+
+
+// want more? 
+// #50
