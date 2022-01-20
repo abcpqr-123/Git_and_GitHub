cce199c5da67b17fba53080b08d74797c43d5284
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-20 22:48:26 +0530

    Add files via upload

---
+++ 06_Working_With_Branches/06_07_More_Practice_Git_Branch.js
@@ -0,0 +1,31 @@
+// NOTE: 
+
+// git create and switch shortcut --> to create a new brach and switch 
+// to that branch in a single command 
+
+//  ==> git switch -c <brach-name> 
+//      Ex: 
+//      git switch -c Surya 
+
+//      SWitched to new branch 'Surya'
+
+
+// nothing to write more.. everything is same as notes in the last 
+// file.. nothing new.. 
+
+// still u want then: 
+// vid - 46
+
+
+// IF U WANT THEN U CAN ALSO CREATE AS MANY FILES AS U WANT IN A PERTICULAR 
+// BRANCH, 
+// GIT WILL ENSURE THAT IT THOSE FILES EXIST IN THE PERTICULAR BRANCH IN WHICH
+// U HAVE CREATED THEM AND NOT IN ANY OTHER BRANCHES. 
+
+// NOTE: 
+// git commit shortcut--> to add and commit in one sigle command 
+// ==> git commit -a -m  "ADD THE MESSAGE"
+
+
+
+
