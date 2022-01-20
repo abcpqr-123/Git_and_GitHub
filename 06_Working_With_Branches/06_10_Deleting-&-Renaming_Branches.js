6c9062e027131a07a487ce96ba23b0e98bdc1c7d
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-20 22:49:05 +0530

    Add files via upload

---
+++ 06_Working_With_Branches/06_10_Deleting-&-Renaming_Branches.js
@@ -0,0 +1,40 @@
+// Note: make sure that u r not in that while deleting it 
+// delete a branch 
+
+// ==> git branch -d <nameOfBranch> 
+
+
+//-------------------------------------------
+
+// delete the branch that is not fully merged: 
+
+// ==> git branch -D <nameOfBranch> 
+
+// THE BRANCH IS NOT FULLY MERGED 
+// if u r sure U WANT to delete the branch then use -D to delete 
+
+
+
+//---------------------------------------------
+
+
+
+
+
+
+
+// RENAMING: 
+
+// ==> git branch -m BRANCH_NAME 
+
+
+// TO RENAME A BRANCH U HAVE TO SWITCH TO THAT BRANCH FIRST 
+// ONLY THEN U CAN RENAME A BRANCH. 
+
+// note: -m --> indicates Move(sounds starnge but its true)
+
+
+
+
+
+
