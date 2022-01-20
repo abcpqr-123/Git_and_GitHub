a490e720b2328f86c30f5706b9e6664197383b32
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-20 22:48:50 +0530

    Add files via upload

---
+++ 06_Working_With_Branches/06_09_Switching_BRanch-Unstaged_Chages.js
@@ -0,0 +1,22 @@
+// before u switch branches its importent that u statge the changes and commit 
+
+// If u wont do that then git will show woring: 
+// * Your local changes to the following file will be over runned  
+// * commit ur changes or stash them before switching branches 
+
+
+// once u commit the changes then u can switch without any complaint 
+// from the git.
+
+
+// Note: note that if u make changes in branch whih conatiains a unique file 
+//       name which no other file or folder has, then there will be no broblem 
+
+//       u can switch the branch. that change which u did not commited 
+//       will follow for every branch u go to 
+
+
+//       BUT STILL ITS ALWAYS BETTER THAT U COMMIT THE CHANGES BEFORE U MOVE 
+//       FROM ONE BRANCH TO ANOTHER BRANCH 
+
+
