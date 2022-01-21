b881e2d8153632e45d3689b11668ccac9e104664
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-21 18:27:22 +0530

    Add files via upload

---
+++ 11_Github_The_Basics/11_03_Why_Use_Github.js
@@ -0,0 +1,32 @@
+// Why one should use github?
+
+// * COLLABORAION 
+// if u ever plan on working on a project with at least one ohter 
+// person, github will make ur life easier! Whether u r building 
+// a hobby project with ur friend or u r collaborating with entire 
+// world, github is essential. 
+
+
+
+// * OPEN SOURCE PROJECTS 
+// Today Github is THE home of open source projects on the internet. 
+// projects ranging from React to Swift are hosted on Github
+
+// If u plan on contributing to open source projets u will need to get 
+// comfortable working with github.
+
+
+
+// * Exposure! 
+// ur Github profile showcases ur own projects and contributions to others 
+// projects. 
+// It an afct as a sort of resume that may employers will consult in the 
+// hiring process. Additionally, u can gain some clout on the plaform for 
+// creating or contributing to popular projects 
+
+
+
+// * Stay Up to Date
+// Being active on Github is the best way to stay up to date with the projects 
+// and tools u rely on. Learn about upcoming changes and the decisions/debate 
+// behind them
