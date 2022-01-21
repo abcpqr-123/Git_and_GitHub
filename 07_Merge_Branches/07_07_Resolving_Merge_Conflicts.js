6d27acc4cb348a693e6c6db918698833f1d0cf44
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-21 15:45:37 +0530

    Add files via upload

---
+++ 07_Merge_Branches/07_07_Resolving_Merge_Conflicts.js
@@ -0,0 +1,25 @@
+// git switch master 
+// git switch -c serina 
+// git switch master 
+// git switch -c bjorn 
+//     make changes in the file songs.txt 
+// git commit -a -m "add song"
+//      if u want make another change in songs.txt and 
+// git commit -a -m "add song 2"
+// git switch serena 
+//     make changes in the file songs.txt 
+// git switch -a -m "made changes"
+//      if u want make another change songs.txt  and 
+// git commit -a -m "add changes 2"
+//     now we have a lot of changes in the same file in the same lines 
+// git switch bjorn
+// git switch -c combo // this file has all the chages made in bjorn 
+// git merge serena 
+//      CONFLICT ARRISES 
+     
+//      now goto text editor 
+//      edit the file in the way u want 
+// git commit -a -m "resolve conflicts"
+
+
+// work completed 
