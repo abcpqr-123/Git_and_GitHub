fc1835baf0dc4cff6e3ca15fa30be10157fbff82
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-21 18:27:07 +0530

    Add files via upload

---
+++ 11_Github_The_Basics/11_02_What_Github_Do_for_Us.js
@@ -0,0 +1,58 @@
+
+// What is Github? 
+
+// Github is a hosting  platform for git repositoties. You can put 
+// ur own Git Repos on github and access them from anywhere and share 
+// them with people around the world 
+
+
+// Beyond hosting repos, Github also provides additional collaborantion
+// features tghat are not native to GIt(but are super useful). Basically, 
+// Github helps people share and collaborate on repos 
+
+
+//----------------------------------------------------
+
+// GIT IS NOT GIUTHB
+
+// Git: 
+// Git is the version control softwere that runs locally on ur machine 
+// You dont need to register for an account. 
+// You don't need to register for an account. 
+// You don't need the internet to use it 
+// You can use git without ever touching github 
+
+
+
+// Github: 
+// Github is asevice that hosts Git repositories in the cloud and makes it 
+// easier toi collaborate with ohter people.
+// You do need to sign up for a account to use Github
+// Its an online place to share work that is done using git 
+
+//-------------------------------------------
+
+// VVV Imp Note: 
+
+// Github is not ur only option 
+// There are tons of competing tools that propvide similar hosting and 
+// collaboration features, including GitLab, BitBucket, and Gerrit 
+
+// But 
+
+// Github IS VERY VERY POPULAR 
+
+// founded in 2008, Github is now the World's largest host of source code. 
+// In early 2020, Github reported having over 40 million users and over 
+// 190 million repositories on the platform.
+
+
+//------------------------------------------------------------
+
+
+// Its Free! 
+
+// Github offers its basic services for free! 
+// While Github does offer paid Team and Enterprise tiers, 
+// the basic Free tier allows for unlimited public and private repos, 
+// unlimited collaborators and more!
