b284891f1dd71acbfac47f8dd5fb0fd42fd47495
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-21 15:44:02 +0530

    Add files via upload

---
+++ 07_Merge_Branches/07_02_Intro_to_Merge.js
@@ -0,0 +1,37 @@
+// Merging: 
+
+// Branching makes it super easy to work within self-connected cointexts 
+// but 
+// often we want to incorparate changes from one branch into another! 
+
+// We merge by using: 
+
+// ==> git merge <branchName>
+
+
+// NOTE: 
+// * WE MERGE BRANCHES, NOT SPECIFIC COMMITS 
+// * WE ALWAYS MERGE TO THE CURRENT HEAD BRANCH (We merge to the branch where we are)
+
+
+
+
+// SETPS OF MERGING: 
+
+// * Switch or checkout the branch u want to merge the changes into 
+//   (the receiving branch)
+
+// ==> git switch master(if u want to merge the file into the master brach) 
+//                      (else in the place of master u add the name of the file 
+//                       which u want)
+
+
+//  after that:
+
+
+// * Use the git merge command to merge changes from a specific branch into 
+//   the current branch. 
+
+// ==> git merge <the Name Of The Branch Which U Want To  Merge> 
+
+  
