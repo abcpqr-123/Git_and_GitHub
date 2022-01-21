fd37d35338c279ea86cb4a4f6608ae4df80cef27
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-21 15:45:52 +0530

    Add files via upload

---
+++ 07_Merge_Branches/07_08_Editing_2_Files_VS_Code.js
@@ -0,0 +1,13 @@
+// imagine that u r on the master branch 
+
+// git switch -c silence 
+//     edit the file 1 and file 2
+//     commit the file 1 and file 2
+// git switch master branch 
+//    edit the file 1 and file 2
+//    commit the file 1 and file 2 
+// git merge silence 
+//    CONFLICT WILL ARISE 
+//    open the text editor and fix the conflict file 1 and file 2
+// git commit
+
