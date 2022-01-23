e9fcf232a7bea0020fde7dc026f607bef382f4d2
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-23 14:57:12 +0530

    Add files via upload

---
+++ 08_Git_Diff_Compare_Changes/08_06_Vierwing_Sttaged_Changes.js
@@ -0,0 +1,10 @@
+// git diff --staged or --cached 
+
+// ==> git diff --staged 
+//        or 
+// ==> git diff --cached 
+// will list the chnages between the staging area and our last commit 
+
+// "Show what will be included in ur commit if u run git commit right now"
+
+
