693c13b199f760fde181c62989478ed1566c2dc1
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-23 14:57:35 +0530

    Add files via upload

---
+++ 08_Git_Diff_Compare_Changes/08_07_Diffing_Specific_Files.js
@@ -0,0 +1,10 @@
+// Diff-ing Specific Files 
+
+// We can view the changes within a specific file by providing git 
+// git diff with a filename. 
+
+// * git diff HEAD [Filename] // to view all staged and unstaged changes.
+// * git diff --staged [Filename]
+
+
+
