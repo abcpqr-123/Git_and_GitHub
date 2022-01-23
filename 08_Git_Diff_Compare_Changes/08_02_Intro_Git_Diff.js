a7383722e02b2fdcd6754355dc865bd58aaca66f
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-23 14:55:19 +0530

    Add files via upload

---
+++ 08_Git_Diff_Compare_Changes/08_02_Intro_Git_Diff.js
@@ -0,0 +1,18 @@
+// We can use the git diff command to view changes between commits, branches, 
+// files, our working direcory and more! 
+
+// We aftern use git diff alongside commands like agit status and git log, 
+// to get a better pcture of a repository and how it has changed over time. 
+
+
+// git diff 
+
+// ==> git diff 
+
+// without additional options, git diff lists all the changes in our working 
+// direcotry that are NOT staged for the next commit 
+
+// gif u hit git diff u will be able see all chnagges made in the files in
+// different colors. 
+
+
