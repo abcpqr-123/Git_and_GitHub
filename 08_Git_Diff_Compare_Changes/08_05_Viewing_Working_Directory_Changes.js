b3f2789a1fb5978c859fa2df113e976f554b8df2
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-23 14:56:56 +0530

    Add files via upload

---
+++ 08_Git_Diff_Compare_Changes/08_05_Viewing_Working_Directory_Changes.js
@@ -0,0 +1,10 @@
+// ==> git diff HEAD
+
+// git diff HEAD lists all changes in the working tree since ur last 
+// commit 
+
+
+// git diff HEAD shows the difference between the HEAD and the working 
+// directory. 
+
+
