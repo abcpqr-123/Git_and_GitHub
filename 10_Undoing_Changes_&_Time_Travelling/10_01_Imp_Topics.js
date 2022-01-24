4d5c75cead42e633a7d5c6dcb1f6103e750cb348
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-24 22:44:51 +0530

    Add files via upload

---
+++ 10_Undoing_Changes_&_Time_Travelling/10_01_Imp_Topics.js
@@ -0,0 +1,29 @@
+// Critical: 
+
+//  Checking out Commits 
+
+//  "Escaping" detached HEAD -
+
+
+// //---------------------------------
+
+
+// Imprortant: 
+
+// Discarding Changes with Checkout 
+
+// Git Restore 
+
+// Git Reset 
+
+// Git Revert 
+
+// //---------------------------------
+
+
+
+// Nice to have: 
+
+
+
+// //---------------------------------
