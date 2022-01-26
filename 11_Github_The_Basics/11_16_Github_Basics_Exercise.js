27847501d0598534e07b18bf8770e623ef69a5f8
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-26 20:20:25 +0530

    Add files via upload

---
+++ 11_Github_The_Basics/11_16_Github_Basics_Exercise.js
@@ -0,0 +1,44 @@
+// mkdir Favorites 
+// cd Favorites 
+// git status 
+// git init 
+//    create new github repo 
+// git remote add origin Url 
+// git branch -M main 
+// touch favorates.txt 
+// git add favorates.txt 
+// git commit -m "add empty favorites file"
+// if u still  see "master" branch name only then run 
+// this command --> git branch -M main 
+// git status 
+// git remote -v 
+// git push origin main 
+// git branch foods 
+// git branch movies 
+// git switch foods 
+// touch foods.txt 
+// code .
+// add someting edit the file and save the file. 
+// git status 
+// git add . 
+// git commit -m "addd favorite fooods"
+// git switch movies 
+// touch movies.txt 
+// code . 
+// add someting edit the file and save the file. 
+// git status 
+// git add . 
+// git commit -m "addd favorite movies"
+// git push origin foods 
+// git push origin movies 
+// git switch main 
+// git merge foods (fast forward merge)
+// git merge movies (conflict arises)
+//     open the text editor, solve the conflict 
+// git status 
+// git add . 
+// git commit -m "merge movies and foods"
+// git log --oneline 
+// git push origin main 
+
+
