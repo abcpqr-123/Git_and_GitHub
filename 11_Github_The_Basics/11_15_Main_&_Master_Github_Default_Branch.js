325ab6d009a63675f557efffe4816d9cd5a38d8b
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-26 20:20:06 +0530

    Add files via upload

---
+++ 11_Github_The_Basics/11_15_Main_&_Master_Github_Default_Branch.js
@@ -0,0 +1,52 @@
+// TO change the brach name in ur github repo master to main   or
+// main to master 
+
+// U CAN ALSO GO TO UR REPO IN GITHUB 
+
+// CLICK ON SETTIGS
+
+// click on BRANCHS 
+
+// switch the default branch name to main to master OR 
+// master to main 
+
+//-------------------------------------------------------------------------------------
+// --------------------------
+
+//  ==> git branch -M main 
+
+//  -------------------------
+
+//  Ex: 
+//  add read me file to it while creating, create a new repo 
+
+//  now it automatically needs to have a branch, and github names
+//  that branch as main and  NOT master 
+
+//  now go to git bash 
+
+//  make sure that u r not in a git repo 
+
+//  ==> git clone (url)
+
+//  now git will create a new bracnh  inside the folder in which u r 
+//  working right now 
+//  to check for that new folder created by git
+//  run command
+
+//  ==> ls 
+//  cd into that folder 
+
+//  now if u look at the branch name, its main 
+
+//  but some times u may need to change the branch name from main to 
+//  master and master to main. 
+
+//  ==> git branch -M main 
+
+//  will rename the brach name from master to main 
+
+//  now to push the branch to main 
+
+//  ==> git push origin main 
+
