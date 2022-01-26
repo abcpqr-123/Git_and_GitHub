acaf2485f01dbe8b6aeed0674d40234ebfa57f2e
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-26 20:17:21 +0530

    Add files via upload

---
+++ 11_Github_The_Basics/11_07_Creating_Github_Repo.js
@@ -0,0 +1,24 @@
+// how do u transfer ur code to git hub? 
+
+// There are two diffrent ways: 
+
+// Option 1: 
+// Existing Repo:
+
+// If u already have an existing repo locally that u want to get 
+// on Github... 
+// * Create a new repo on Github 
+// * Connect ur local repo (add a remote)
+// * Push up ur changes to github 
+
+
+
+// Option 2: 
+// Start From Scratch 
+
+// If u have not begun work on ur local repo, u can...
+// * Create a brand ne4w repo on github 
+// * Clone it down tgo ur machine 
+// * Do some work locally 
+// * Push up ur changes to Github 
+
