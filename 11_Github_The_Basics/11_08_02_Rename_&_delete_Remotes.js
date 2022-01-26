7fbf925838e46e865f125833e3cd86a3470b1c72
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-26 20:18:02 +0530

    Add files via upload

---
+++ 11_Github_The_Basics/11_08_02_Rename_&_delete_Remotes.js
@@ -0,0 +1,10 @@
+// OTHER COMMADNDS: 
+
+// They are not commanly used, but there are commands to rename 
+// and delete remotes if needed 
+
+// Renameing the remote        
+// git remote rename <old> <new>
+
+// DELETING THE REMOTE
+// git remote remove <name>
