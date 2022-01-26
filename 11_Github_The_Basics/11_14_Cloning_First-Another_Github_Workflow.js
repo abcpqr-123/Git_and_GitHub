c9937030c801c259cc3c218fa2906e48f6d839df
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-26 20:19:52 +0530

    Add files via upload

---
+++ 11_Github_The_Basics/11_14_Cloning_First-Another_Github_Workflow.js
@@ -0,0 +1,50 @@
+// Option 2: 
+// Start From Scratch 
+
+// If u have not begun work on ur local repo, u can...
+// * Create a brand ne4w repo on github 
+// * Clone it down tgo ur machine 
+// * Do some work locally 
+// * Push up ur changes to Github 
+
+
+// create a new repo 
+
+// cd in to the repo where u want to clone, 
+
+// copy the url of the git hub project 
+
+// and hit 
+
+// ==> git clone (url u copied)
+
+// now git will create a new direcotry inside ur current directory 
+
+// check for that directory by runnig the command 
+
+// ==> ls 
+
+// cd into init 
+
+// if u hit git status u will know that its a git repo 
+
+// it will also have pre-configured remote called origin 
+
+// u can always check for that by 
+
+// ==> git remote -v 
+
+// nw u can create files 
+// ==> touch dogs.txt 
+
+// edit the file using text editor 
+
+// add and commit the file. 
+
+// after addintg all the file now u can push it to Github 
+
+// ==> git push origin master
+
+// if u now go and refresh the github repo u will find the file u 
+// added to the github. in this case u will see dog.txt file. 
+
