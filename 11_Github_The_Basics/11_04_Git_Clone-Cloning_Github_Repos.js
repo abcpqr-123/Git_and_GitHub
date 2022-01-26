b1c9627044527625ad136c069469c0af930d222d
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-01-26 20:16:01 +0530

    Add files via upload

---
+++ 11_Github_The_Basics/11_04_Git_Clone-Cloning_Github_Repos.js
@@ -0,0 +1,46 @@
+// git clone 
+
+// ==> git clone <url> 
+
+// To clone a repo, run ==> git clone <url> (u have to copy the url from github)
+
+// Git will retrieve all the files associated with the repository 
+// and will copy them to ur local machine 
+
+// In addition, Git initializes a new repository on ur machine, 
+// giving u caccess to the full Git history of the cloned project 
+
+// Ex: 
+// imagine that ur in folder called cloning, 
+// Note: make sure that git is not initialized in the folder in which u r 
+// running the commad 
+// to check that hit command ==> git status , 
+
+// and once u r sure that there is not initialized ,
+
+// u hit command
+// ==> git clone <url>
+
+// Note: whereever u run git clone, that is the place or folder where 
+// git is going to create a new folder for u. 
+
+// and u NEED INTERNET for this, 
+
+// now a new folder will be created inside cloning folder by Git, 
+
+// if u 
+// ==> cd 
+// into that foder, and hit 
+// ==> ls 
+// u can see the files and folders of that github repository which u 
+// clolned. 
+
+// and it will be a git repo, 
+
+// if u hit
+// ==>  git log , 
+// u will be able to see the full commit history of that project 
+
+
+
+
